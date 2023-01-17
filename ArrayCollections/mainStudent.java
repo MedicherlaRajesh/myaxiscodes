b1072a190a2177c3cc36fd901e0078c0ef955491
MedicherlaRajesh <121540549+MedicherlaRajesh@users.noreply.github.com>
2023-01-17 22:36:59 +0530

    Add files via upload

---
+++ ArrayCollections/mainStudent.java
@@ -0,0 +1,26 @@
+package ArrayCollections;
+import java.util.Scanner;
+public class mainStudent {
+    public static void main(String[] args) {
+        Student obj1 = new Student();
+        System.out.println("Enter the size of array : ");
+        int n = obj1.sc.nextInt();
+        obj1.arrayStudent = new Student[n];
+        while(true) {
+            System.out.println("Enter case\n 1:addStudent\n 2:display\n 3:exit\n ");
+            int choice = obj1.sc.nextInt();
+            switch(choice) {
+                case 1:
+                    obj1.addStudent();
+                    break;
+                case 2:
+                    obj1.displayStudent();
+                    break;
+                case 3:
+                    System.exit(1);
+
+            }
+    
+        }
+    }
+}
